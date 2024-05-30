08341d56a6de9714db8ee94c19d2f000e94f7152
zhyck1316 <zhyck1316@gmail.com>
2024-05-30 13:31:14 +0800

    Updated path

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["/**/*.{html,js}"],
+  content: ['./index.html', './**/*.{html,js}'],
   theme: {
     extend: {
       colors:{
